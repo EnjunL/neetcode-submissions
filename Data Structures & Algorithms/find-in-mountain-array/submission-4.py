7f20797b451592a8b78b323e92e36d0a7000c84b
EnjunL <72420505+EnjunL@users.noreply.github.com>
2026-06-18 12:17:23 -0700

    Add: find-in-mountain-array - submission-4

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-4.py
@@ -0,0 +1,52 @@
+#im tuff
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        #Find Peak 
+        #check m, then go left or right parition
+        #example arr Peak=4  [1,2,3, 4 ,3,2,1]
+        length = mountainArr.length()
+        l,r = 0, length-1
+        res = float("inf")
+
+        #Find Peak
+        while l<r:
+            m = (l+r)//2
+            mVal = mountainArr.get(m)
+            
+            #increasing side
+            if mVal < mountainArr.get(m+1):
+                l = m+1
+            #decreasing side
+            else:
+                r=m
+        
+        #peak is at r
+        #check left first
+        peak = r
+        l=0
+        while l<=r:
+            m = (l+r)//2
+            mVal = mountainArr.get(m)
+
+            if mVal == target:
+                return m
+            elif mVal > target:
+                r = m-1
+            else:
+                l = m+1
+        #check right is res
+        if res == float("inf"):
+            l,r=peak+1,length-1
+            while l<=r:
+                m = (l+r)//2
+                mVal = mountainArr.get(m)
+
+                if mVal == target:
+                    return m
+                elif mVal < target:
+                    r = m-1
+                else:
+                    l=m+1
+        return -1
+
+
