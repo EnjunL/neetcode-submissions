36dcd7d82b78b65d83b5e8f97ffc0949bece9209
EnjunL <72420505+EnjunL@users.noreply.github.com>
2026-08-23 23:25:23 -0700

    Add: delete-node-in-a-bst - submission-1

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-1.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return
+        if key < root.val:
+            root.left = self.deleteNode(root.left,key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right,key)
+        # we have found the key
+        else:
+            #no left child
+            if not root.left:
+                return root.right
+            #no right child
+            elif not root.right:
+                return root.left
+            # both children exist; we can use either the
+            # smallest node in the right / largest node in the left subtree            
+            #i choose right side, find smallest of the right tree so that when I replace
+            #the deleted node with it, the BST still holds.
+            else:
+                #picking the right side
+                successor = root.right
+
+                #finding the smallest value of the right side
+                while successor.left:
+                    successor=successor.left
+                
+                #replace value of current node(which is the node we want to delete)
+                # with the successor node
+                root.val = successor.val
+
+                #delete the node we originally copied from starting from the head of the
+                #right subtree
+                root.right = self.deleteNode(root.right,successor.val)
+
+        return root
